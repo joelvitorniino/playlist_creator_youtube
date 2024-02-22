9c384f66e3d6c2a05b522e3e2aebcbd94e02215e
Joel Vitor Niino Campos <joelprofessor13@gmail.com>
2024-02-22 19:11:52 -0300

    feature: add signs to error or success

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 "use client";
 
+import Head from "next/head";
 import { FormEvent, useState, useEffect } from "react";
 import useSWR from "swr";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import LinkInvalid from "./component/LinkInvalid";
+import LinkSuccess from "./component/LinkSuccess";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
@@ -14,24 +20,49 @@ export default function Home() {
   const [audioUrls, setAudioUrls] = useState<string[]>([]);
   const [currentAudioIndex, setCurrentAudioIndex] = useState(0);
   const { data, error } = useSWR<DataType>(
-    `http://localhost:3000/api/playlist?url=${url}`,
+    url ? `http://localhost:3000/api/playlist?url=${url}` : null,
     fetcher
   );
 
+  const [showSuccess, setShowSuccess] = useState(false);
+  const [isUrlSubmitted, setIsUrlSubmitted] = useState(false);
+
+  const notifyError = () => toast.error("Link invalid or try again.");
+  const notifySuccess = () =>
+    toast.success("Link has been linked to playlist.");
+
   useEffect(() => {
-    if (data?.urlMusic && !audioUrls.includes(data.urlMusic)) {
+    if (data?.urlMusic && !audioUrls.includes(data.urlMusic) && isUrlSubmitted) {
       setAudioUrls((prevUrls) => [...prevUrls, data.urlMusic]);
+      notifySuccess();
+      setShowSuccess(true);
+
+      // Reset isUrlSubmitted to false after processing
+      setIsUrlSubmitted(false);
+
+      // Hide success message after 6 seconds
+      const timer = setTimeout(() => setShowSuccess(false), 6000);
+      return () => clearTimeout(timer);
     }
-  }, [data, audioUrls]);
+  }, [data, audioUrls, isUrlSubmitted]);
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setUrl(e.currentTarget.url.value);
+
+    // Indicate that a URL has been submitted
+    setIsUrlSubmitted(true);
   };
 
   const handleAudioEnd = () => {
     setCurrentAudioIndex((prevIndex) => (prevIndex + 1) % audioUrls.length);
   };
+
+  useEffect(() => {
+    if (error) {
+      notifyError();
+    }
+  }, [error]);
 
   useEffect(() => {
     if (audioUrls.length > 1 && currentAudioIndex !== 0) {
@@ -46,6 +77,10 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen justify-center items-center bg-gray-100">
+      <Head>
+        <title>Playlist Creator</title>
+      </Head>
+
       <div className="max-w-md mx-auto p-8 bg-white rounded shadow-md">
         <h1 className="text-3xl font-semibold text-center mb-6 text-blue-500">
           Playlist Creator
@@ -64,6 +99,11 @@ export default function Home() {
             Adicionar
           </button>
         </form>
+
+        {error && <LinkInvalid />}
+
+        {showSuccess && <LinkSuccess />}
+
         {audioUrls.length > 0 && (
           <audio
             id="audioElement"
